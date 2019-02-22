2f45626d4d64905d7d992f20b57b99858714ba8b
Chris McGinnis <chris.mcginnis@ucsf.edu>
2019-02-22 11:26:40 -0800

    Add files via upload

---
+++ R/MULTIseq.Align.Suite.R
@@ -0,0 +1,235 @@
+
+###############################################################################################################################
+## MULTIseq.align aligns barcode reads to a reference in 10000-cell buckets, returning a cellIDs x ref barcode count matrix  ##
+## 'read.table' is a cellID x 3 dataframe with Cell, UMI, and Sample columns                                                 ##
+## 'read.table' must be pre-subsetted to include only cell barcode reads present in cellIDs                                  ##
+## 'cellIDs' is a vector of sequenced cellIDs desired for alignment; 'ref' is a vector of reference sample barcode sequences ##                                                    ##
+###############################################################################################################################
+MULTIseq.align <- function(readTable, cellIDs, ref) {
+  require(stringdist)
+
+  ## Bucket cell IDs
+  print("Bucketing cell IDs...")
+  cellIDs_bucketed <- bucket_cellIDs(cellIDs)
+
+  ## Bucket read.table
+  print("Bucketing read tables...")
+  readTable_bucketed <- bucket_readTable(readTable, cellIDs_bucketed)
+
+  ## Initialize output
+  bar.table <- as.data.frame(matrix(0L, ncol=length(ref)+2, nrow=length(cellIDs)))
+  colnames(bar.table) <- c(paste("Bar", 1:length(ref),sep=""), "nUMI", "nUMI_total")
+  rownames(bar.table) <- cellIDs
+
+  ## Iterate through cell ID and readTable buckets
+  for (bucket in 1:length(cellIDs_bucketed)) {
+    print(paste("Aligning bucket #",bucket,"...",sep=""))
+    t0 <- Sys.time()
+    print(Sys.time())
+
+    cellIDs.temp <- cellIDs_bucketed[[bucket]]
+    readTable.temp <- readTable_bucketed[[bucket]]
+
+    for (cell in cellIDs.temp) {
+      ## Extract umis and tags from reads with specific cellID
+      r1.ind <- which(readTable.temp$Cell == cell)
+      umis <- readTable.temp$UMI[r1.ind]
+      tags <- readTable.temp$Sample[r1.ind]
+
+      ## Record total number of reads
+      bar.table[cell, "nUMI_total"] <- length(umis)
+
+      ## Find tags with HD <= 1 from any reference tag
+      tag.dists <- stringdistmatrix(a=tags, b=ref)
+
+      tag.hds <- apply(tag.dists, 1, min)
+      tag.ind <- which(tag.hds <= 1)
+      if (length(tag.ind) == 0) { next } ## Skip cellIDs with 0 aligned read (bug fix)
+      tag.dists <- tag.dists[tag.ind, ]
+
+      ## Remove duplicated UMIs for reads with reference-matching tags
+      umis <- umis[tag.ind]
+      umi.ind <- which(duplicated(umis) == FALSE)
+
+      ## Subset by unique UMIs, record total number of unique UMIs, fill count table with HD <= 1
+      if (length(umi.ind) == 1) {
+        if (is.null(ncol(tag.dists)) == TRUE) { tag.dists <- tag.dists[umi.ind] }
+        if (is.null(ncol(tag.dists)) == FALSE) { tag.dists <- tag.dists[umi.ind, ] }
+        bar.table[cell,"nUMI"] <- length(umi.ind)
+
+        for (tag in 1:length(tag.dists)) {
+          bar.table[cell,tag] <- length(which(tag.dists[tag] <= 1))
+        }
+      }
+
+      if (length(umi.ind) > 1) {
+        tag.dists <- tag.dists[umi.ind, ]
+        bar.table[cell,"nUMI"] <- length(umi.ind)
+
+        for (tag in 1:ncol(tag.dists)) {
+          bar.table[cell,tag] <- length(which(tag.dists[,tag] <= 1))
+        }
+      }
+    }
+
+    print(paste("Done aligning bucket #",bucket,"...",sep=""))
+    print(Sys.time()-t0)
+
+  }
+
+  return(bar.table)
+
+}
+
+##########################################################################################
+## bucket_cellIDs is an internal function, sets up 10000-member cellID buckets          ##
+## 'cellIDs' is a character vector of cellIDs desired in the final barcode count matrix ##
+##########################################################################################
+bucket_cellIDs <- function(cellIDs) {
+
+  bucket.ind <- seq(1, length(cellIDs), by=10000)
+  cellID.list <- list()
+
+  for (bucket in 1:length(bucket.ind)) {
+    if (bucket != length(bucket.ind)) {
+      cellIDs.temp <- cellIDs[bucket.ind[bucket]:(bucket.ind[bucket+1]-1)]
+      cellID.list[[bucket]] <- cellIDs.temp
+    }
+
+    if (bucket == length(bucket.ind)) {
+      cellIDs.temp <- cellIDs[bucket.ind[bucket]:length(cellIDs)]
+      cellID.list[[bucket]] <- cellIDs.temp
+    }
+  }
+
+  return(cellID.list)
+
+}
+
+#############################################################################################
+## bucket_readTable is an internal function, splits read data according to cellID buckets  ##
+## 'readTable' is a parsed read table, as provided by 'MULTIseq.preProcess'                ##
+## 'cellIDs_bucketed' is a bucketed list of cellIDs, as provided by 'bucket_cellIDs'       ##
+#############################################################################################
+bucket_readTable <- function(readTable, cellIDs_bucketed) {
+
+  readTable.list <- list()
+
+  for (bucket in 1:length(cellIDs_bucketed)) {
+    cellIDs.temp <- cellIDs_bucketed[[bucket]]
+    ind <- which(readTable$Cell %in% cellIDs.temp)
+    readTable.list[[bucket]] <- readTable[ind, ]
+  }
+
+  return(readTable.list)
+
+}
+
+#########################################################################################################
+## MULTIseq.preProcess reads in raw barcode FASTQs and allocates reads into cell barcode,              ##
+## UMI, and sample barcode subsets. Function parses FASTQs to only include desired cellIDs             ##
+## 'R1' is an R1 FASTQ file; 'R2' is an R2 FASTQ file; 'cellIDs' is a character vecotro of cellIDs     ##
+#########################################################################################################
+MULTIseq.preProcess <- function(R1, R2, cellIDs) {
+  require(ShortRead)
+
+  r1 <- readFastq(R1)
+  r2 <- readFastq(R2)
+  readTable <- cbind(as.data.frame(subseq(sread(r1),1,16)),
+                     as.data.frame(subseq(sread(r1),17,26)),
+                     as.data.frame(subseq(sread(r2),1,8)))
+  colnames(readTable) <- c("Cell","UMI","Sample")
+  ind <- which(readTable$Cell %in% cellIDs)
+  readTable <- readTable[ind, ]
+
+  return(readTable)
+}
+
+#########################################################################################################
+## MULTIseq.preProcess_allCells reads in raw barcode FASTQs and allocates reads into cell barcode,     ##
+## UMI, and sample barcode subsets Function buckets data for pre-processing all present barcodes       ##
+## 'R1' is an R1 FASTQ file; 'R2' is an R2 FASTQ file; 'cellIDs' is the 10X cell barcode whitelist     ##
+#########################################################################################################
+MULTIseq.preProcess_allCells <- function(R1, R2, whitelist) {
+  require(ShortRead)
+
+  print("Reading in R1...")
+  r1 <- readFastq(R1)
+  print("Reading in R2...")
+  r2 <- readFastq(R2)
+  print("Assembling read table...")
+  readTable <- cbind(as.data.frame(subseq(sread(r1),1,16)),
+                     as.data.frame(subseq(sread(r1),17,26)),
+                     as.data.frame(subseq(sread(r2),1,8)))
+  colnames(readTable) <- c("Cell","UMI","Sample")
+
+  print("Bucketing cell IDs...")
+  cellIDs_bucketed <- bucket_cellIDs(whitelist)
+
+  ## Initialize logicals for storing present Cell IDs, cell-associated reads
+  present.cells <- logical(length(whitelist))
+  cell.linked.reads <- logical(nrow(readTable))
+
+  ## Iterate through cell ID buckets, finding reads associated with each cell
+  x <- length(cellIDs_bucketed)
+  for (bucket in 1:x) {
+    print(paste("Starting bucket ",bucket,"/",x,sep=""))
+    t0 <- Sys.time()
+    print(Sys.time())
+
+    cellID.counter <- 10000*(bucket-1)
+    cellIDs.temp <- cellIDs_bucketed[[bucket]]
+
+    read.ind <- which(readTable$Cell %in% whitelist)
+    cell.linked.reads[read.ind] <- TRUE
+
+    cell.ind <- which(whitelist %in% readTable$Cell)
+    present.cells[(cell.ind+cellID.counter)] <- TRUE
+  }
+
+  print("Parsing final cell IDs...")
+  cellIDs.parsed <- whitelist[present.cells]
+  assign(x="cellIDs.parsed", value=cellIDs.parsed, envir = .GlobalEnv)
+
+  print("Parsing final read table...")
+  readTable.parsed <- readTable[cell.linked.reads, ]
+  return(readTable.parsed)
+}
+
+
+
+##############################################################################
+## 'alignRate' computes the proportion of reads that align to the reference ##
+##############################################################################
+alignRate <- function(readTable, cellIDs, ref) {
+  require(stringdist)
+
+  print("Subsetting readTable...")
+  ind <- which(readTable$Cell %in% cellIDs)
+  readTable <- readTable[ind, ]
+
+  ## Compute align rate
+  print("Computing alignment rate...")
+  align.rate <- rep(0L, length(cellIDs))
+  counter <- 0
+  for (cell in cellIDs) {
+    counter <- counter + 1
+    print(counter)
+    ## Extract umis and tags from reads with specific cellID
+    r1.ind <- which(readTable$Cell == cell)
+    umis <- readTable$UMI[r1.ind]
+    tags <- readTable$Sample[r1.ind]
+
+    ## Compute HD for all tags relative to reference
+    tag.dists <- stringdistmatrix(a=tags, b=ref)
+
+    ## Find rate at which reads align to reference
+    tag.hds <- apply(tag.dists, 1, min)
+    x <- (length(which(tag.hds <= 1))/length(tag.hds))*100
+    align.rate[counter] <- x
+  }
+
+  return(align.rate)
+
+}
+
---
+++ R/MULTIseq.Classification.Suite.R
@@ -0,0 +1,323 @@
+###########################################
+## MULTI-seq sample classification suite ##
+###########################################
+
+###################
+## classifyCells ##
+###################
+classifyCells <- function(barTable, q) {
+  require(KernSmooth)
+
+  ## Normalize Data: Log2 Transform, mean-center
+  barTable.n <- as.data.frame(log2(barTable))
+  for (i in 1:ncol(barTable)) {
+    ind <- which(is.finite(barTable.n[,i]) == FALSE)
+    barTable.n[ind,i] <- 0
+    barTable.n[,i] <- barTable.n[,i]-mean(barTable.n[,i])
+  }
+
+  ## Pre-allocate memory to save processing time/memory
+  n_BC <- ncol(barTable.n) # Number of barcodes
+  n_cells <- nrow(barTable.n) # Numer of cells
+  bc_calls <- rep("Negative",n_cells) # Barcode classification for each cell
+  names(bc_calls) <- rownames(barTable.n)
+
+  ## Generate thresholds for each barcode:
+  ## 1. Gaussian KDE with bad barcode detection, outlier trimming
+  ## 2. Define local maxima for GKDE
+  ## 3. Split maxima into low and high subsets, adjust low if necessary
+  ## 4. Threshold and classify cells according to user-specified inter-maxima quantile
+
+  for (i in 1:n_BC) {
+    ## Step 1: GKDE
+    model <- tryCatch( { approxfun(bkde(barTable.n[,i], kernel="normal")) },
+                       error=function(e) { print(paste0("No threshold found for ", colnames(barTable.n)[i],"...")) } )
+    if (class(model) == "character") { next }
+    x <-  seq(from=quantile(barTable.n[,i],0.001), to=quantile(barTable.n[,i],0.999), length.out=100)
+
+    ## Step 2: Local maxima definition
+    extrema <- localMaxima(model(x))
+
+    if (length(extrema) <= 1) {
+      print(paste0("No threshold found for ", colnames(barTable.n)[i],"..."))
+      next
+    }
+
+    ## Step 3: Select maxima
+    ## Assumes negative cells are largest mode
+    ## Assumes positive cells are highest extreme -- favors negatives over doublets
+    low.extreme <- extrema[which.max(model(x)[extrema])]
+    high.extreme <- max(extrema)
+    if (low.extreme == high.extreme) {print(paste0("No threshold found for ", colnames(barTable.n)[i],"...")) }
+
+    ## Step 4: Threshold and classify cells
+    thresh <- quantile(c(x[high.extreme], x[low.extreme]), q)
+    cell_i <- which(barTable.n[,i] >= thresh)
+    n <- length(cell_i)
+    if (n == 0) { next } # Skip to next barcode if no cells classified
+    bc_calls[cell_i] <- sapply(bc_calls[cell_i],
+                               FUN = function(x) {
+                                 if (x == "Negative") {
+                                   return(colnames(barTable.n)[i])
+                                 } else {
+                                   return("Doublet")
+                                 } })
+  }
+
+  return(bc_calls)
+
+}
+
+################
+## findThresh ##
+################
+findThresh <- function(call.list, id) {
+  require(reshape2)
+
+  res <- as.data.frame(matrix(0L, nrow=length(call.list), ncol=4))
+  colnames(res) <- c("q","pDoublet","pNegative","pSinglet")
+  q.range <- unlist(strsplit(names(call.list), split="q="))
+  res$q <- as.numeric(q.range[grep("0", q.range)])
+  nCell <- length(call.list[[1]])
+
+  for (i in 1:nrow(res)) {
+    temp <- table(call.list[[i]])
+    if ( "Doublet" %in% names(temp) == TRUE ) { res$pDoublet[i] <- temp[which(names(temp) == "Doublet")] }
+    if ( "Negative" %in% names(temp) == TRUE ) { res$pNegative[i] <- temp[which(names(temp) == "Negative")] }
+    res$pSinglet[i] <- sum(temp[which(names(temp) != "Doublet" & names(temp) != "Negative")])
+  }
+
+  res <- melt(res, id.vars="q")
+  res[,4] <- res$value/nCell
+  colnames(res)[2:4] <- c("Subset","nCells","Proportion")
+  assign(x=paste("res_",id,sep=""), value=res, envir = .GlobalEnv)
+
+  extrema <- res$q[localMaxima(res$Proportion[which(res$Subset == "pSinglet")])]
+  assign(x=paste("extrema_",id,sep=""), value=extrema, envir = .GlobalEnv)
+}
+
+#################
+## localMaxima ##
+#################
+localMaxima <- function(x) {
+  # Use -Inf instead if x is numeric (non-integer)
+  y <- diff(c(-.Machine$integer.max, x)) > 0L
+  rle(y)$lengths
+  y <- cumsum(rle(y)$lengths)
+  y <- y[seq.int(1L, length(y), 2L)]
+  if (x[[1]] == x[[2]]) {
+    y <- y[-1]
+  }
+  y
+}
+
+######################
+## findReclassCells ##
+######################
+findReclassCells <- function(barTable, neg.cells) {
+  require(KernSmooth)
+
+  ## Normalize Data: Log2 Transform, mean-center
+  print("Normalizing barode data...")
+  barTable.n <- as.data.frame(log2(barTable))
+  for (i in 1:ncol(barTable)) {
+    ind <- which(is.finite(barTable.n[,i]) == FALSE)
+    barTable.n[ind,i] <- 0
+    barTable.n[,i] <- barTable.n[,i]-mean(barTable.n[,i])
+  }
+  barTable.n_neg.cells <- barTable.n[neg.cells, ]
+
+  ## Pre-allocate memory to save processing time/memory
+  print("Pre-allocating data structures...")
+  n_BC <- ncol(barTable.n)
+  bcs <- colnames(barTable.n)
+  n_neg <- length(neg.cells)
+  bc.mats <- list()
+  q.range <- seq(0.01, 0.99, by=0.02)
+  ref.mat <- as.data.frame(matrix(0L, nrow=n_neg, ncol=length(q.range)))
+  rownames(ref.mat) <- neg.cells
+  colnames(ref.mat) <- paste("q", q.range, sep="_")
+  for (i in 1:n_BC) {
+    bc.mats[[i]] <- ref.mat
+  }
+  names(bc.mats) <- bcs
+
+  ## Iterate through BCs, modeling distribution using GKDE
+  ## Define low and high maxima before testing all negative cells across all q
+  print("Determining classifications for negative cells across all BCs, all q...")
+  for (i in 1:n_BC) {
+    ## Step 1: GKDE
+    model <- approxfun(bkde(barTable.n[,i], kernel="normal"))
+    x <- seq(from=quantile(barTable.n[,i],0.001), to=quantile(barTable.n[,i],0.999), length.out=100)
+
+    ## Step 2: Local maxima definition
+    extrema <- localMaxima(model(x))
+
+    ## Step 3: Select maxima, avoiding noisy extremes (if possible)
+    ## Assumes negative cells are largest mode
+    ## Assumes positive cells are highest extreme -- favors negatives over doublets
+    low.extreme <- extrema[which.max(model(x)[extrema])]
+    high.extreme <- max(extrema)
+
+    ## Iterate through q, tracking classiication status for negative cells
+    col.ind <- 0
+    for (q in q.range) {
+      col.ind <- col.ind + 1
+      thresh <- quantile(c(x[high.extreme], x[low.extreme]), q)
+      cell_i <- which(barTable.n_neg.cells[,i] >= thresh)
+      bc.mats[[i]][cell_i, col.ind] <- 1
+    }
+  }
+
+  ## Determine classification status across q.range for negative cells across all barcodes
+  print("Computing classification stability...")
+  bc.mat.final <- Reduce('+',bc.mats)
+
+  ## Compute classification stability ~ i.e., number of q for which cell has 1 classification
+  res <- as.data.frame(matrix(0L, nrow=n_neg, ncol=2))
+  colnames(res) <- c("ClassStability","Reclassification")
+  rownames(res) <- neg.cells
+  res$Reclassification <- "Negative"
+  for (cell in neg.cells) {
+    res[cell,"ClassStability"] <- length(which(bc.mat.final[cell, ] == 1))
+  }
+
+  ## Extract classifications for cells above cluster.stability reclassification threshold
+  # Define reclassifiable cells
+  print(paste("Extracting rescued classifications...",sep=""))
+  cells2reclass <- neg.cells[which(res$ClassStability >= 1)]
+
+  # Find q at which reclassifiable cells have single classification, extract corresponding bc
+  for (cell in cells2reclass) {
+    q.ind <- max(which(bc.mat.final[cell, ] == 1))
+
+    for (bar in 1:n_BC) {
+      temp <- bc.mats[[bar]][cell, q.ind]
+      if (temp == 1) { res[cell, "Reclassification"] <- bcs[bar] }
+    }
+  }
+
+  # Return only cells with an available classification
+  res <- res[-which(res$Reclassification == "Negative"), ]
+  return(res)
+
+}
+
+#################
+## rescueCells ##
+#################
+rescueCells <- function(barTable, classifications, reclassifications) {
+
+  ## Extract subset of data with equal numbers of high-confidence classifications
+  smallest.bc <- min(table(classifications)) # Find least-frequent BC classification
+  bcs <- colnames(barTable)
+  conf.cells <- NULL
+  for (bc in bcs) { # Get n cellIDs for each bc, where n is number of cells in least-frequent BC classification
+    temp <- names(classifications)[which(classifications == bc)]
+    temp <- sample(temp, smallest.bc, replace=FALSE)
+    conf.cells <- c(conf.cells, temp)
+  }
+  conf.dat.classifications <- classifications[conf.cells]
+
+  ## Normalize data including high-confidence and negative cells prior to k-means
+  kmeans.dat <- barTable[c(conf.cells, rownames(reclassifications)), ]
+  kmeans.dat <- as.data.frame(log2(kmeans.dat))
+  for (i in 1:ncol(kmeans.dat)) {
+    ind <- which(is.finite(kmeans.dat[,i]) == FALSE)
+    kmeans.dat[ind,i] <- 0
+    kmeans.dat[,i] <- kmeans.dat[,i]-mean(kmeans.dat[,i])
+  }
+
+  ## Initialize k-means results output
+  conf.res.temp <- as.data.frame(matrix(0L, nrow=length(conf.cells), ncol=3))
+  colnames(conf.res.temp) <- c("GT","GT.adj","KMEANS")
+  rownames(conf.res.temp) <- conf.cells
+  conf.res.temp$GT <- factor(conf.dat.classifications, levels=colnames(barTable))
+  conf.res.temp$GT.adj <- as.numeric(conf.res.temp$GT)
+
+  neg.res.list <- list()
+  for (iter in 1:smallest.bc) {
+    neg.res.temp <- reclassifications
+    neg.res.temp$Reclassification <- factor(neg.res.temp$Reclassification, levels=colnames(barTable))
+    neg.res.temp[,"Reclass.adj"] <- as.numeric(neg.res.temp$Reclassification)
+    neg.res.temp[, "KMEANS"] <- rep(0L)
+    neg.res.list[[iter]] <- neg.res.temp
+  }
+
+  ## Initial match rate results output
+  conf.match.rate <- rep(0L, smallest.bc)
+  neg.match.rate <- as.data.frame(matrix(0L, nrow=max(neg.res.temp$ClassStability), ncol=3))
+  colnames(neg.match.rate) <- c("ClassStability", "MatchRate_mean","MatchRate_sd")
+  neg.match.rate$ClassStability <- 1:nrow(neg.match.rate)
+
+  ## Perform k-means iterations and match rate computations
+  for (iter in 1:smallest.bc) {
+    ## Set centers
+    print(iter)
+    n_BC <- ncol(barTable)
+    centers.ind <- seq(1, smallest.bc*n_BC, by=smallest.bc) + (iter-1)
+    centers <- kmeans.dat[centers.ind, ]
+
+    ## Run k-means, store results for confident and negative cells
+    kmeans_temp <- kmeans(kmeans.dat, centers = centers)
+    conf.res.temp$KMEANS <- kmeans_temp$cluster[1:length(conf.cells)]
+    neg.res.list[[iter]]$KMEANS <- kmeans_temp$cluster[(length(conf.cells)+1):length(kmeans_temp$cluster)]
+
+    ## Compute match rate for confident cells
+    conf.match.rate[iter] <- 1-(length(which(conf.res.temp$GT.adj != conf.res.temp$KMEANS))/nrow(conf.res.temp))
+  }
+
+  ## Compute match rate statistics across classification stabilities
+  for (cs in 1:nrow(neg.match.rate)) {
+    ind <- which(neg.res.list[[1]]$ClassStability == cs)
+    match.rate.means <- NULL
+
+    for (iter in 1:smallest.bc) {
+      neg.res.temp <- neg.res.list[[iter]][ind, ]
+      x <- 1-(length(which(neg.res.temp$Reclass.adj != neg.res.temp$KMEANS))/nrow(neg.res.temp))
+      match.rate.means <- c(match.rate.means,x)
+    }
+
+    neg.match.rate$MatchRate_mean[cs] <- mean(match.rate.means)
+    neg.match.rate$MatchRate_sd[cs] <- sd(match.rate.means)
+
+  }
+
+  res <- rbind(c(100,mean(conf.match.rate),sd(conf.match.rate)),neg.match.rate)
+  return(res)
+
+}
+
+#############
+## barTSNE ##
+#############
+barTSNE <- function(barTable) {
+  require(Rtsne)
+
+  ## Normalize barcode count matrix
+  n_BC <- ncol(barTable)
+  barTable.n <- as.data.frame(log2(barTable))
+  for (i in 1:n_BC) {
+    ind <- which(is.finite(barTable.n[,i]) == FALSE)
+    barTable.n[ind,i] <- 0
+    barTable.n[,i] <- barTable.n[,i]-mean(barTable.n[,i])
+  }
+
+  ## Run tSNE
+  tsne.res <- Rtsne(barTable.n, dims=2, initial_dims=n_BC, verbose=TRUE, check_duplicates=FALSE, max_iter=2500)
+  tsne.embedding <- as.data.frame(tsne.res$Y)
+  colnames(tsne.embedding) <- c("TSNE1","TSNE2")
+  tsne.embedding[,3:(n_BC+2)] <- barTable.n
+  rownames(tsne.embedding) <- rownames(barTable)
+
+  return(tsne.embedding)
+}
+
+###########
+## findQ ##
+###########
+findQ <- function(res, extrema) {
+  temp <- res[which(res$q %in% extrema & res$Subset == "pSinglet"), ]
+  x <- as.numeric(temp$q[which.max(temp$Proportion)])
+  return(x)
+}
